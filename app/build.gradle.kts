965444669255e465dd39a02fba1cb4ee299c5f70
Nguyen Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2026-04-30 15:07:33 +0700

    build: 1.5.0 (6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "app.pwhs.universalinstaller"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 6
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
