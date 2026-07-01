39e2a115b2fa928af79b5e39b791b9a89f4f354a
Nguyen Quang Minh <minhnq1@talent.apero.vn>
2026-07-01 18:24:06 +0700

    Fix Shizuku mode hidden API bypass on Android 9+

--- app/src/main/java/app/pwhs/universalinstaller/util/ShizukuDefaultInstaller.kt
+++ app/src/main/java/app/pwhs/universalinstaller/util/ShizukuDefaultInstaller.kt
@@ -25,7 +25,7 @@ object ShizukuDefaultInstaller {
             runCatching {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                     HiddenApiBypass.addHiddenApiExemptions(
-                        "Landroid/content/pm/IPackageManager;",
+                        "Landroid/content/pm/IPackageManager"
                     )
                 }
                 val packageBinder = SystemServiceHelper.getSystemService("package")
